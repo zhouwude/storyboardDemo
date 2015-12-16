17b9b8890fb312d359129e8fb224357821e79299
zhouwude <572240202@qq.com>
2015-12-16 11:44:44 +0800

    www

--- StoryBookDemo/secondViewController.m
+++ StoryBookDemo/secondViewController.m
@@ -17,7 +17,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.secField.text = self.data;
-    // Do any additional setup after loading the view.
+    UIView *view = [[UIView alloc] init];
+    view.backgroundColor = [UIColor redColor];
+    [self.view addSubview:view];
+    view.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.topLayoutGuide attribute:NSLayoutAttributeBottom multiplier:1 constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeft multiplier:1 constant:100]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:0 constant:100]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:0 constant:100]];
+    //
+    UIView *view2 = [[UIView alloc] init];
+    view2.backgroundColor = [UIColor redColor];
+    [self.view addSubview:view2];
+    view2.translatesAutoresizingMaskIntoConstraints = NO;
+    // 这玩意就相遇顶部的 navibar 隐藏 就相当于消失 id类型 self。bottomGuide相当于底部的 bar
+    id top = self.topLayoutGuide;
+    NSDictionary *dic = NSDictionaryOfVariableBindings(view2,top);
+    NSDictionary *dic1 = @{@"hh":@(20)};
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[top]-hh-[view2(100)]" options:0 metrics:dic1 views:dic]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-300-[view2(20)]" options:0 metrics:dic1 views:dic]];
+
+}
+-(BOOL)prefersStatusBarHidden{
+    return YES;
+    
 }
 - (IBAction)enter:(id)sender {
     thirdViewController *vc = [[thirdViewController alloc] initWithNibName:@"thirdViewController" bundle:nil];
